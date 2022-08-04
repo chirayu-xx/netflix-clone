884cdf7f2ae11aee6621b032d3f327f3293e5200
chirayu-xx <84080202+chirayu-xx@users.noreply.github.com>
2022-08-04 18:03:09 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,7 +39,7 @@ function Header() {
        </div>
        <div className='flex items-center space-x-4 text-sm font-light'>
            <SearchIcon className='hidden sm:inline h-6 w-6'/>
-           <p className='hidden lg:inline'>Kids</p>
+           <p className='hidden lg:inline'>By Chirayu</p>
            <BellIcon className='h-6 w-6'/>
            {/* <Link href='/account'> */}
            <img onClick={logout}
